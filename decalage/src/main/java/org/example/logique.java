539abcaee04d4a533503f0e01d94fbe785390744
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-13 10:14:36 +0200

    update moyen

---
+++ decalage/src/main/java/org/example/logique.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class logique {
+    public  static void main(){
+        String[] fruits = new String[] { "D", "E", "C","A","L","A","G","E" };
+    }
+}
--- tpTab/src/main/java/org/example/Tableau.java
+++ tpTab/src/main/java/org/example/Tableau.java
@@ -2,11 +2,15 @@ package org.example;
 
 public class Tableau {
     public static void getArray() {
-        int maxVal = Integer.MAX_VALUE;
-        int minVal = Integer.MIN_VALUE;
-
-        int [] note={10,10,10,10,10,0,0,0,0,0};
+        int minVal ;
+        int maxVal   ;
+          //tableau
+        int [] note={10,10,10,10,10,2,2,2,2,1};
+        //initialisation
+        minVal=note[0];
+        maxVal=note[0];
         System.out.println(note[1]);
+        //boucle 
         for (int init =0 ;init<note.length;init++){
             System.out.println(note[init]);
         }
@@ -17,13 +21,14 @@ public class Tableau {
         float moyenne = (float) somme / note.length;
 
         for(int i = 0; i < note.length; i++){
-            if(note[i] < maxVal)
-                maxVal = note[i];
-            if(note[i] > minVal)
+            if(note[i] < minVal)
                 minVal = note[i];
+            if(note[i] > maxVal)
+                maxVal = note[i];
         }
 
         System.out.println("la moyen est de "+moyenne);
+        System.out.println("le plus petit est de " + minVal);
         System.out.println("le plus petit est de " + maxVal);
 
 }};
