74a7b913d64cdfc6592f46507cef25cfbc41ee38
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-14 09:20:35 +0200

    tp justePrix

---
+++ Juste_Prix/src/main/java/org/example/Logique.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Logique {
+    public static void getPrint(){
+        int NbrRandom = (int) (Math.random() * 100);
+        System.out.println(NbrRandom);
+        /*
+        Scanner scan = new Scanner(System.in);
+        System.out.println("veuillez entrer un nombre");
+        int nbr =scan.nextInt();
+        System.out.println(nbr);*/
+    }
+}
---
+++ rectangle2/src/main/java/org/example/Logique.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Logique {
+    public static void getPrint(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("veuillez entrer  une hauteur pour le rectangle ");
+        String hauteur = scan.next();
+        System.out.println(hauteur);
+        System.out.println("veuillez entrer  une largeur pour le rectangle ");
+        String largeur = scan.next();
+        System.out.println(largeur);
+
+
+    }
+}
