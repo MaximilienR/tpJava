622a1f4c114db07171dfa27b8657773d939155f3
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-14 10:01:16 +0200

    update juste prix

--- Juste_Prix/src/main/java/org/example/Logique.java
+++ Juste_Prix/src/main/java/org/example/Logique.java
@@ -6,10 +6,43 @@ public class Logique {
     public static void getPrint(){
         int NbrRandom = (int) (Math.random() * 100);
         System.out.println(NbrRandom);
-        /*
+
         Scanner scan = new Scanner(System.in);
         System.out.println("veuillez entrer un nombre");
-        int nbr =scan.nextInt();
-        System.out.println(nbr);*/
+        int NbrUser =scan.nextInt();
+        System.out.println(NbrUser);
+
+        if (NbrUser==NbrRandom){
+            System.out.println("bravo vous avez trouvé le juste prix");
+        }else while (NbrUser!=NbrRandom){
+            if (NbrUser<NbrRandom){
+                System.out.println("veuillez entrer un nombre plus grand");
+                int Nbr =scan.nextInt();
+                break;
+            }else if (NbrUser>NbrRandom){
+                System.out.println("veuillez entrer un nombre plus petit");
+                int Nbr =scan.nextInt();
+                break;
+
+            }
+        }
     }
 }
+       /* while (NbrUser!=NbrRandom)
+        { if (NbrUser<NbrRandom){
+            System.out.println("veuillez entrer un nombre plus grand");
+
+            while (NbrUser<NbrRandom){
+             //   int NbrUser =scan.nextInt();
+            }break;
+        }else if (NbrUser>NbrRandom){
+            System.out.println("veuillez entre un nombre pls petit");
+            break;
+        }else System.out.println(
+                "bravo vous avez trouver le juste prix "
+        );
+        }
+
+        }
+
+}*/
