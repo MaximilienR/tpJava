c768dc511af7783fcf0b84600e80d6bbbb7665d9
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-14 10:38:03 +0200

    update juste prix

--- Juste_Prix/src/main/java/org/example/Logique.java
+++ Juste_Prix/src/main/java/org/example/Logique.java
@@ -4,45 +4,35 @@ import java.util.Scanner;
 
 public class Logique {
     public static void getPrint(){
+        //je crée un nombre aléatoire
         int NbrRandom = (int) (Math.random() * 100);
         System.out.println(NbrRandom);
-
+        //je demande un nombre à l'utilisateur
         Scanner scan = new Scanner(System.in);
         System.out.println("veuillez entrer un nombre");
         int NbrUser =scan.nextInt();
-        System.out.println(NbrUser);
+        //System.out.println(NbrUser);
+        //je traitre les différents cas possibile
 
+        //si nombre juste fin programe
         if (NbrUser==NbrRandom){
             System.out.println("bravo vous avez trouvé le juste prix");
+        //tant que nombre différent je fais une des deux solution suivante
         }else while (NbrUser!=NbrRandom){
+            //je demande à l'utilisateur un nombre plus grand
             if (NbrUser<NbrRandom){
                 System.out.println("veuillez entrer un nombre plus grand");
                 int Nbr =scan.nextInt();
-                break;
+                //NbrUser==Nbr;
+
+                //break;
+                //je demande un nombre plus petit
             }else if (NbrUser>NbrRandom){
                 System.out.println("veuillez entrer un nombre plus petit");
                 int Nbr =scan.nextInt();
-                break;
 
             }
         }
     }
 }
-       /* while (NbrUser!=NbrRandom)
-        { if (NbrUser<NbrRandom){
-            System.out.println("veuillez entrer un nombre plus grand");
-
-            while (NbrUser<NbrRandom){
-             //   int NbrUser =scan.nextInt();
-            }break;
-        }else if (NbrUser>NbrRandom){
-            System.out.println("veuillez entre un nombre pls petit");
-            break;
-        }else System.out.println(
-                "bravo vous avez trouver le juste prix "
-        );
-        }
-
-        }
-
-}*/
+       //pb : il prends en compte le nouveau chiffre ??
