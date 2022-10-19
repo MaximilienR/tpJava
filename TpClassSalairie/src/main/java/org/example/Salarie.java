79fd7723ca32e28d70d910f852c63ed5c8bfc6e6
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-19 16:04:55 +0200

    tp

--- TpClassSalairie/src/main/java/org/example/Salarie.java
+++ TpClassSalairie/src/main/java/org/example/Salarie.java
@@ -3,14 +3,15 @@ package org.example;
 public class Salarie {
 
     private int matricule;
-    private int id;
+    private int id=0;
+    public static int count;
     private String categorie;
     private  String service;
     private String nom;
     private double salaire;
 
     public Salarie(int id,int matricule, String categorie, String service, String nom,double salaire) {
-        this.id=id;
+        this.id=++count;
         this.matricule = matricule;
         this.categorie = categorie;
         this.service =service;
