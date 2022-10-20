fba3e95b8b6ad3b132f247cd20128f12a4df412f
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-20 09:42:27 +0200

    creat class

--- Tp_Employe/src/main/java/org/example/Employe.java
+++ Tp_Employe/src/main/java/org/example/Employe.java
@@ -6,6 +6,15 @@ public class Employe {
     private double salaire;
 
     public Employe(String nom,int age,double salaire) {
+        this.nom=nom;
+        this.age=age;
+        this.salaire=salaire;
+    }
+    public void afficher(){
+        System.out.println(this.nom +" " +this.age+"an "+" "+this.salaire+"€");
+    }
+    public void calculeSalire(){
 
     }
+
 }
--- Tp_Employe/src/main/java/org/example/Main.java
+++ Tp_Employe/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employe employeRandom = new Employe("Smith",33,1200.99);
+        employeRandom.afficher();
     }
 }
