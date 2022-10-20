2ea04a5af6237fab13ba115738843aeafe425f46
Maximilien Roussel <ro.maximilien@gmail.com>
2022-10-20 09:14:23 +0200

    first commit

---
+++ Tp_Employe/src/main/java/org/example/Employe.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Employe {
+    private String nom;
+    private int age;
+    private double salaire;
+
+    public Employe(String nom,int age,double salaire) {
+
+    }
+}
