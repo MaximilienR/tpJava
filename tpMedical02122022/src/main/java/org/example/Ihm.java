bb25aae0158476a1606023ead5fbf55bbad6afbc
Maximilien Roussel <ro.maximilien@gmail.com>
2022-12-02 10:25:02 +0100

    start creat menu

--- tpMedical02122022/src/main/java/org/example/Ihm.java
+++ tpMedical02122022/src/main/java/org/example/Ihm.java
@@ -5,4 +5,25 @@ import java.util.Scanner;
 public class Ihm {
 
     private Scanner scanner;
+
+    public  Ihm(){
+        scanner = new Scanner(System.in);
+    }
+
+    public  void start() {
+        String choice;
+        do {
+            menu();
+            choice=scanner.nextLine();
+        } while (!choice.equals("0"));
+    }
+
+
+    /////////////////MENU/////////////////////////////
+
+    public void menu(){
+        System.out.println("----------MENU-----------");
+        System.out.println("0 -- QUITTER LE PROGRAMME");
+        System.out.println();
+    }
 }
