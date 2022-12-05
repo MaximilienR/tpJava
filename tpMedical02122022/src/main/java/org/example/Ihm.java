ffa6ece3b9236949d83285e76d084017830f4e6f
Maximilien Roussel <ro.maximilien@gmail.com>
2022-12-05 11:24:40 +0100

    add choice sexe patient

--- tpMedical02122022/src/main/java/org/example/Ihm.java
+++ tpMedical02122022/src/main/java/org/example/Ihm.java
@@ -48,12 +48,30 @@ public class Ihm {
         //System.out.println("l'addresse du patient est "+ " "+addressePatient);
         System.out.println("veuillez saisir votre numero de telephone portable");
         int numTel = scanner.nextInt();
-        System.out.println("le numero du patient est "+ " "+"0"+numTel);
+        //System.out.println("votre num est donc" + " " +"0"+numTel);
+        System.out.println("veuillez saisir m si vous êtes un homme ou f si vous êtes une femme");
+        char sexe = scanner.next().charAt(0);
+        // System.out.println("vous etes donc de sexe  "+" "+ sexe);
+      //  System.out.println("donc si j'ai bien compris vous vous applez "+" "+nomPatient+" "+prenomPatient+" "+" vous êtes de sexe"+ " " + sexe+ "vous habitez à l'addresse :"+ " "+addressePatient+ " " + "votre numero de telephone est le "+ " "+ "0"+ numTel);
+        System.out.println("donc si j'ai bien compris vous vous applez :"+" "+nomPatient+" "+prenomPatient);
+        System.out.println("vous êtes de sexe :  "+sexe);
+        System.out.println("vous habitez à l'addresse :"+addressePatient);
+        System.out.println("enfin votre numero de telephone est le : "+ " "+"0"+numTel);
     }
     /*prenom,nom,date,sexe,addresse,numTel*/
     private void  AfficherdossierPatient(){
         System.out.println("veuillez saisir votre numero de dossier");
     }
+   /*
+    Test choix du sexe
+   private  void test(){
+        System.out.println("etes vous : ");
+
+        System.out.println("A : un homme ");
+        System.out.println("B : une femme ");
+        String sexe =scanner.nextLine();
+        System.out.println("vous êtes donc "+sexe );
+    }*/
 
 
 }
