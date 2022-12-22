8be2ac690ec6568d8a572913f0c01f255ccb3b33
Maximilien Roussel <ro.maximilien@gmail.com>
2022-12-22 12:21:07 +0100

    creat IDAO

---
+++ tpHotelSpring/src/main/java/com/example/tphotelspring/interfaces/IDAO.java
@@ -0,0 +1,13 @@
+package com.example.tphotelspring.interfaces;
+
+import java.util.List;
+
+public interface IDAO<T> {
+    boolean create(T o);
+    boolean update(T o);
+    boolean delete(T o);
+
+    T findById(int id);
+
+    List<T> findAll();
+}
