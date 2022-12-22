a9fc36a9cccec50bb01a0260b7abd50f99d9dae9
Maximilien Roussel <ro.maximilien@gmail.com>
2022-12-22 15:26:40 +0100

    update entity

--- tpHotelSpring/src/main/java/com/example/tphotelspring/entity/Client.java
+++ tpHotelSpring/src/main/java/com/example/tphotelspring/entity/Client.java
@@ -1,16 +1,25 @@
 package com.example.tphotelspring.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "client")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
     private  String nom;
     private  String prenom;
     private  String telephone;
+
+    public  Client(String nom,String prenom,String telephone){
+        this.setNom(nom);
+        this.setPrenom(prenom);
+        this.setTelephone(telephone);
+    }
 
 }
