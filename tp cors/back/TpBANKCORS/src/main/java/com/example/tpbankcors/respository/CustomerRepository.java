6283f452066fdfeb3abc8d150a5346031b3cfa4a
Maximilien Roussel <ro.maximilien@gmail.com>
2023-01-13 11:37:56 +0100

    search function

--- tp cors/back/TpBANKCORS/src/main/java/com/example/tpbankcors/respository/CustomerRepository.java
+++ tp cors/back/TpBANKCORS/src/main/java/com/example/tpbankcors/respository/CustomerRepository.java
@@ -1,11 +1,20 @@
 package com.example.tpbankcors.respository;
 
 import com.example.tpbankcors.entity.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer,Integer> {
+
+
+    //methode de recherche
+    @Query("select c from Customer c where c.name like :keyword")
+    List<Customer> findByName(@Param("keyword") String keywords);
 
 }
 
