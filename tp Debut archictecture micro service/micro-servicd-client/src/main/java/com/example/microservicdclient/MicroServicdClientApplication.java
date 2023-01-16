ede5723914183de61e3eedceb0736ab2e12a021f
Maximilien Roussel <ro.maximilien@gmail.com>
2023-01-16 15:03:13 +0100

    tp1 debut architecture micro-service

---
+++ tp Debut archictecture micro service/micro-servicd-client/src/main/java/com/example/microservicdclient/MicroServicdClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicdclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicdClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicdClientApplication.class, args);
+	}
+
+}
---
+++ tp Debut archictecture micro service/micro-servicd-client/src/test/java/com/example/microservicdclient/MicroServicdClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicdclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicdClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
