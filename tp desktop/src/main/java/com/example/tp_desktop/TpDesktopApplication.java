0187ea294733b2eda6c2c62bd77e812380571918
Maximilien Roussel <ro.maximilien@gmail.com>
2023-02-16 12:06:42 +0100

    first commit

--- tp desktop/src/main/java/com/example/tp_desktop/TpDesktopApplication.java
+++ tp desktop/src/main/java/com/example/tp_desktop/TpDesktopApplication.java
@@ -3,7 +3,7 @@ package com.example.tp_desktop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+ 
 public class TpDesktopApplication {
 
 	public static void main(String[] args) {
