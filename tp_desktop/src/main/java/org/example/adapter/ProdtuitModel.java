b4798a30290792904c5fe63557702f792a6ebd9a
Maximilien Roussel <ro.maximilien@gmail.com>
2023-02-17 11:44:54 +0100

    11h44

--- tp_desktop/src/main/java/org/example/adapter/ProdtuitModel.java
+++ tp_desktop/src/main/java/org/example/adapter/ProdtuitModel.java
@@ -1,4 +1,20 @@
 package org.example.adapter;
 
-public class ProdtuitModel {
+import javax.swing.table.AbstractTableModel;
+
+public class ProdtuitModel extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
 }
--- tp_desktop/src/main/java/org/example/window/MenuPanel.java
+++ tp_desktop/src/main/java/org/example/window/MenuPanel.java
@@ -3,9 +3,13 @@ package org.example.window;
 import lombok.Data;
 
 import javax.swing.*;
+import java.awt.*;
 
 @Data
 public class MenuPanel {
+    private JPanel mainPanel;
+    private GridLayout mainGridLayout;
+
     private JButton testButton;
     private JPanel menu;
 
