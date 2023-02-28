1afaae4971e47b43b619a1cad32e9cb80f4a65d8
Maximilien Roussel <ro.maximilien@gmail.com>
2023-02-28 16:33:58 +0100

    update activity 2

--- app/src/main/java/com/example/tp_ecf_android/MainActivity.java
+++ app/src/main/java/com/example/tp_ecf_android/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.tp_ecf_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
- 
+ Button b1;
+ ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.b1);
+      //  list= findViewById(R.id.list);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"vous allez êtres rediregé",Toast.LENGTH_SHORT).show();
+            }
+            Intent intent = new Intent(MainActivity.this,PannierActivity.class);
+
+        });
+
     }
 }
